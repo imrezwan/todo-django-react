7c1e53f6d236886984c156ed7260163b30f5851f
rezwan2525 <rezwan2525@gmail.com>
2022-08-13 17:11:06 +0600

    BE - get all todos done initailly

---
+++ backend/todos/views.py
@@ -0,0 +1,9 @@
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoListView(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
