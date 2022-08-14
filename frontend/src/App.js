4c25db101963f4de68faf5a08140e82b9074c96b
rezwan2525 <rezwan2525@gmail.com>
2022-08-14 10:17:39 +0600

    FE - show mock data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+      "id": 1,
+      "title": "Django TODO API",
+      "body": "both FE and BE will be covered in this section"
+  },
+  {
+      "id": 2,
+      "title": "Django Blog API",
+      "body": "It's like a practice section where we will cover all the learning"
+  },
+  {
+    "id": 3,
+    "title": "Java Programming",
+    "body": "It's java learning"
+  }
+]
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {list};
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.list.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
