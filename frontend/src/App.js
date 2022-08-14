f63f9bbe9318b2e7ed1e70489b9d30767d2abff9
rezwan2525 <rezwan2525@gmail.com>
2022-08-14 10:30:46 +0600

    FE - fetch backend data with axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
 const list = [
@@ -20,15 +21,30 @@ const list = [
 ]
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {list};
+
+  state = {
+    todos: [],
+  }
+
+  componentDidMount() {
+    this.getTodos();
+  }
+
+  getTodos() {
+    axios
+      .get('http://127.0.0.1:8000/api/')
+      .then( res => {
+        this.setState( {todos: res.data });
+      })
+      .catch( err => {
+        console.log(err);
+      });
   }
 
   render() {
     return (
       <div>
-        {this.state.list.map(item => (
+        {this.state.todos.map(item => (
           <div key={item.id}>
             <h1>{item.title}</h1>
             <p>{item.body}</p>
